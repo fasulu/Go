db15ceea15142570ea42fee26b1c3e81a8db27b5
fasulu <rfasulu@hotmail.com>
2021-07-30 21:17:10 +0200

    Maps

--- goTutorial/main.go
+++ goTutorial/main.go
@@ -4,17 +4,43 @@ import (
 	"fmt"
 )
 
-var score = 19.2	// to use this score variable, it needs to be declared outside function
-
 func main() {
 
-	// package scope, using variables declared in different go packages
+	// Maps - used to store values in array with keys and values
 
-	sayHello("ray")
-
-	for _, v := range names {
-		fmt.Println((v))
+	menu := map[string]float64 {
+		
+		"soup" : 1.99,
+		"salad" : 0.99,
+		"water" : 0.49,
+		"coffee" : 0.99,
 	}
 
-	showScore()
+	fmt.Println(menu["soup"])	// gives 1.99
+	fmt.Println(menu)			// gives map[coffee:0.99 salad:0.99 soup:1.99 water:0.49]
+
+	//
+
+	phonelist := map[int]string {
+		54664654: "ray",
+		98765754: "sha",
+		23541845: "bee",
+	}
+
+	fmt.Println(phonelist[54664654])
+	fmt.Println(phonelist)
+
+	// looping maps
+
+	for k,v := range phonelist {
+		fmt.Println(k, "-", v)			// result will be 54664654 - ray
+														//98765754 - sha
+														//23541845 - bee
+	}
+
+	// updating the map
+
+	phonelist[54664654] = "rayh"	// change value of 54664654 to rayh
+
+	fmt.Println(phonelist)		//result is map[23541845:bee 54664654:rayh 98765754:sha]
 }
