e1cd1da58b3c9825372def130632dfd1967f9d4f
fasulu <rfasulu@hotmail.com>
2021-08-01 15:49:42 +0200

    Example to create a bill and using Receiver Function

--- goTutorial/bill.go
+++ goTutorial/bill.go
@@ -5,36 +5,50 @@ import (
 )
 
 type bill struct {
-	name string
+	name  string
 	items map[string]float64
-	tip float64
+	tip   float64
 }
 
 // create a new bill
 
 func newBill(name string) bill {
-	b := bill {
-		name:name,
-		items: map[string]float64{"cake" : 3.99, "water" : 0.49},
-		tip:0,
+	b := bill{
+		name:  name,
+		// items: map[string]float64{"cake": 3.99, "water": 0.49},
+		items: map[string]float64{},
+		tip:   0,
 	}
 	return b
 }
 
 // formatting the bill
 
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
 	// list items
 	for key, value := range b.items {
-		fs += fmt.Sprintf("%-10v ...$%v \n", key+":", value)	// -10 will reserves 10 characters long space for each keys, the prints value  example cake:      ...$3.99
+		fs += fmt.Sprintf("%-15v ...$%v \n", key+":", value) // -15 will reserves 15 characters long space for each keys, the prints value  example cake:      ...$3.99
 		total += value
 	}
 
-	// total
-	fs += fmt.Sprintf("%-10v ...$%0.2f", "total:", total)	// -10 will reserves 10 characters long space for each keys, the prints value  example total:       ...$4.48
+	// add tip
+	fs += fmt.Sprintf("%-15v ...$%0.2f \n", "tip:", b.tip)
 	
+	// total
+	fs += fmt.Sprintf("%-15v ...$%0.2f", "total:", total+b.tip) // -15 will reserves 15 characters long space for each keys, the prints value  example total:       ...$4.48
+
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+// add an item to the bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- goTutorial/main.go
+++ goTutorial/main.go
@@ -10,10 +10,18 @@ func main() {
 	// Struct means structure. Is used for custom build structure for data object 
 	// ie blue print of data stucture which is equivalent to class objects in other object oriented languages
 	// Here we make a custom build data structure called bill 
+	// Using Receiver Functions
 
 	mybill := newBill("ray's bill")
 
 	fmt.Println(mybill)	// creates a new bill using the bill.go. result is {ray's bill map[] 0}
+
+	mybill.addItem("cake", 3.99)
+	mybill.addItem("water", 0.49)
+	mybill.addItem("coffee", 1.99)
+	mybill.addItem("hot choclat", 2.49)
+
+	mybill.updateTip(10)
 	
 	fmt.Println(mybill.format())
 }
